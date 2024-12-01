1707f1c8426e0d14f67ff444f9dfe9cfbf116eed
sonu kumar <sonu36437@gmail.com>
2024-12-01 19:55:57 +0530

    make ui more understanding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ const ContactSection = () => {
     text: ''
   });
   const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [serverMessage, setServerMessage]=React.useState("");
+  const [sending ,setSending]=React.useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -14,20 +16,30 @@ const ContactSection = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('https://my-mailer-iota.vercel.app/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: formData.email,
-        text: formData.text,
-        name:formData.name
-      }),
-    });
-
-    if (response.ok) {
+    setSending(true);
+    try {
+      const response = await fetch('https://my-mailer-iota.vercel.app/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          text: formData.text,
+          name: formData.name
+        }),
+      });
+      const data = await response.json(); // Get the response data
+       setServerMessage(data.message);// Log the server message
+      setSending(false);
       setIsModalOpen(true);
+
+      if (response.ok) {
+        setIsModalOpen(true);
+      }
+    } catch (error) {
+      console.error('Error:', error); // Log any errors
+      setSending(false);
     }
   };
 
@@ -51,7 +63,7 @@ const ContactSection = () => {
               whileFocus={{ scale: 1.02 }}
             />
             <motion.input
-              type="text"
+              type="email"
               name="email"
               placeholder="Email"
               className="bg-gray-800 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-[#AFE1AF]"
@@ -73,7 +85,7 @@ const ContactSection = () => {
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
           >
-            Send Message
+           { !sending?"Send Message":"sending..."}
           </motion.button>
         </form>
       </motion.div>
@@ -81,7 +93,7 @@ const ContactSection = () => {
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white text-black p-6 rounded-lg">
-            <h2 className="text-lg font-bold">Mail has been sent!</h2>
+            <h2 className="text-lg font-bold">{serverMessage}</h2>
             <button onClick={() => setIsModalOpen(false)} className="mt-4 bg-yellow-400 text-black py-2 px-4 rounded">
               Close
             </button>
